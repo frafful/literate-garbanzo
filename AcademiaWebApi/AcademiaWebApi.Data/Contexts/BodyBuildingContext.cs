f08b2a80d7daacd14be404b0eb65e3a9d1965867
frafful <felipe.rafful@gmail.com>
2016-02-15 22:48:38 -0200

    Inicio da implementacao do repositorio e unidade de trabalho

---
+++ AcademiaWebApi/AcademiaWebApi.Data/Contexts/BodyBuildingContext.cs
@@ -0,0 +1,24 @@
+﻿using AcademiaWebApi.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.ModelConfiguration.Conventions;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AcademiaWebApi.Data.Contexts
+{
+    public class BodyBuildingContext : DbContext
+    {
+        public BodyBuildingContext() : base("BodyBuildingContext")
+        { }
+
+        public IDbSet<Muscle> Muscle { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+            modelBuilder.Conventions.Remove<PluralizingTableNameConvention>();
+        }
+    }
+}
---
+++ AcademiaWebApi/AcademiaWebApi.Data/Repositories/MuscleRepository.cs
@@ -0,0 +1,17 @@
+﻿using AcademiaWebApi.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AcademiaWebApi.Data.Contexts;
+
+namespace AcademiaWebApi.Data.Repositories
+{
+    public class MuscleRepository : GenericRepository<Muscle>, IMuscleRepository
+    {
+        public MuscleRepository(BodyBuildingContext context) : base(context)
+        {
+        }
+    }
+}
